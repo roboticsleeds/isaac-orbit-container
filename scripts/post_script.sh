23628c792fb83ae0daa8f01a5e5d1c90d924de8a
Rafael Papallas <rpapallas@gmail.com>
2023-06-28 13:13:52 +0100

    Installed VS Code, useful to be used with IsaacSim built-in code view.

--- scripts/post_script.sh
+++ scripts/post_script.sh
@@ -39,6 +39,11 @@ apt install -y --force-yes \
 rosdep init
 rosdep update
 
+# Install VS Code
+cd /tmp
+curl -o code.deb -L http://go.microsoft.com/fwlink/?LinkID=760868
+apt-get install -y ./code.deb
+
 # Let's have a custom PS1 to help people realise in which container they are
 # working.
 CUSTOM_ENV=/.singularity.d/env/99-zz_custom_env.sh
